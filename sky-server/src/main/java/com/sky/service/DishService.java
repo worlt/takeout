1cc8700bdcda0cc78cc671fefd2e222b67aa1f17
worlt <2516930419@qq.com>
2025-03-20 17:51:47 +0800

    feat(admin): 新增菜品分页查询和批量删除功能
    - 在 DishController 中添加了菜品分页查询和批量删除的接口
    - 在 DishService 和 DishServiceImpl 中实现了分页查询和批量删除的逻辑
    - 在 DishMapper 和 DishFlavorMapper 中添加了相应的 SQL 方法
    - 新增 SetmealDishMapper 接口和 XML 文件，用于查询菜品关联的套餐信息- 优化了 WebMvcConfiguration 中的 API 文档配置

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,10 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
 
 /**
  * @Author worlt
@@ -8,4 +12,18 @@ import com.sky.dto.DishDTO;
  */
 public interface DishService {
     void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     * @return
+     */
+    void deleteBatch(List<Long> ids);
 }
